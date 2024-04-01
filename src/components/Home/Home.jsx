628e2e9db7ffe3ee446e5d186e3efb919ef4ccfe
Timo Blumer <timo.blumer@gewa.ch>
2024-04-01 21:27:07 +0200

    updatejfiosj-fksfi

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -30,20 +30,26 @@ const Home = () => {
                 <h2>{slide.title}</h2>
                 <p>{slide.description}</p>
               </div>
-          
-          
-              
             </div>
           ))}
         </div>
-      
       </div>
       <div className='text-container'>
-                  <div className='titel'>
-                    <h1>Unsere Dienstleistungen</h1>
-                    <p>Wir möchten dich herzlich einladen zu unserer Hochzeit.</p>
-                  </div>
-                </div>
+        <div className='titel'>
+          <h1>Unsere Dienstleistungen</h1>
+        </div>
+        <div className='text'>
+          <p>
+            Wir bieten eine breite Palette von Dienstleistungen an, die Ihre Bedürfnisse erfüllen. Von der Webseitenprogrammierung bis zur Erstellung professioneller Diashows stehen wir Ihnen zur Verfügung.
+          </p>
+          <p>
+            Unser erfahrenes Team wird sicherstellen, dass Ihre Anforderungen mit hoher Qualität und Präzision erfüllt werden.
+          </p>
+          <p>
+            Kontaktieren Sie uns noch heute, um mehr über unsere Dienstleistungen zu erfahren und wie wir Ihnen helfen können.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
