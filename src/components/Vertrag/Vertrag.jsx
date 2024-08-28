b7a67435704201b1c05d58f8e79e4902d991a25c
Timo Blumer <timo.blumer@gewa.ch>
2024-08-28 09:09:56 +0200

    Udjfsf

--- src/components/Vertrag/Vertrag.jsx
+++ src/components/Vertrag/Vertrag.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
 import './Vertrag.scss';
-import { FaCopy } from 'react-icons/fa'; // Importiere das Copy-Icon von react-icons
+import { FaCopy } from 'react-icons/fa';
 
 const Vertrag = () => {
   const { id } = useParams();
@@ -54,10 +54,28 @@ const Vertrag = () => {
     doc.text(`Der Auftraggeber hat das Recht, die Anforderungen an die Dienstleistungen jederzeit zu ändern, sofern dies schriftlich mitgeteilt wird. Änderungsanforderungen können sich auf Design, Funktionalität, Inhalte und weitere Aspekte der Dienstleistung beziehen.`, 10, 90, { maxWidth: 180 });
     doc.text(`Der Auftragnehmer verpflichtet sich, unverzüglich auf Änderungsanforderungen zu reagieren und eine schriftliche Einschätzung der Auswirkungen auf den Umfang, die Zeitplanung und die Leistungen zu erstellen.`, 10, 105, { maxWidth: 180 });
 
-    if (selectedKunde.auftragsTyp === 'Webseite') {
+    if (selectedKunde.auftragsTyp === 'Webseite Programmieren') {
       doc.text('3. Admin-Rolle und Bearbeitung der Webseite', 10, 125);
       doc.text(`Der Auftraggeber erhält das Recht, die Webseite in seiner Funktion als Administrator zu bearbeiten und Änderungen vorzunehmen. Dies umfasst die Möglichkeit, Inhalte, Bilder, Texte, Videos und andere Informationen auf der Webseite eigenständig zu verwalten und zu aktualisieren.`, 10, 135, { maxWidth: 180 });
       doc.text(`Der Auftragnehmer wird dem Auftraggeber eine benutzerfreundliche und sichere Admin-Oberfläche zur Verfügung stellen, die es dem Auftraggeber ermöglicht, Änderungen an der Webseite vorzunehmen, ohne die Integrität oder Funktionsfähigkeit der Webseite zu beeinträchtigen.`, 10, 150, { maxWidth: 180 });
+
+      doc.text('4. Rücktrittsrecht des Auftraggebers', 10, 170);
+      doc.text(`Der Auftraggeber hat das Recht, vom Vertrag zurückzutreten, wenn der Auftragnehmer wesentliche Vertragsverpflichtungen nicht erfüllt. Im Falle eines Rücktritts wird eine angemessene Entschädigung für bereits erbrachte Leistungen fällig.`, 10, 180, { maxWidth: 180 });
+
+      doc.text('5. Einsicht in den Source Code', 10, 200);
+      doc.text(`Der Auftraggeber hat das Recht, Einsicht in den Source Code der Webseite zu nehmen, um die Qualität und Übereinstimmung der Programmierung zu überprüfen. Der Zugriff auf den Source Code wird dem Auftraggeber in einem angemessenen Umfang gewährt.`, 10, 210, { maxWidth: 180 });
+
+      doc.text('6. Berichtspflicht', 10, 230);
+      doc.text(`Der Auftragnehmer verpflichtet sich, dem Auftraggeber regelmäßig über den Fortschritt der Arbeiten zu berichten. Diese Berichte umfassen den Stand der Umsetzung, eventuelle Probleme und die voraussichtliche Fertigstellung.`, 10, 240, { maxWidth: 180 });
+
+      doc.text('7. Vertraulichkeit', 10, 260);
+      doc.text(`Der Auftragnehmer verpflichtet sich, alle vom Auftraggeber bereitgestellten Informationen und Daten vertraulich zu behandeln und diese nicht ohne ausdrückliche Zustimmung des Auftraggebers an Dritte weiterzugeben.`, 10, 270, { maxWidth: 180 });
+
+      doc.text('8. Urheberrecht und Eigentum', 10, 290);
+      doc.text(`Alle Urheberrechte und geistigen Eigentumsrechte an der von dem Auftragnehmer erstellten Webseite verbleiben bis zur vollständigen Bezahlung beim Auftragnehmer. Nach vollständiger Bezahlung gehen die Rechte an der Webseite und ihren Inhalten auf den Auftraggeber über.`, 10, 300, { maxWidth: 180 });
+
+      doc.text('9. Vergütung', 10, 320);
+      doc.text(`Die Vergütung für die Erstellung und Betreuung der Webseite wird in Anlage 2 festgelegt. Zahlungen sind gemäß dem vereinbarten Zahlungsplan zu leisten. Bei Zahlungsverzug behält sich der Auftragnehmer vor, die Arbeit bis zur Begleichung der ausstehenden Beträge einzustellen.`, 10, 330, { maxWidth: 180 });
     }
 
     doc.save('Vertrag.pdf');
@@ -66,7 +84,7 @@ const Vertrag = () => {
   const copyCode = () => {
     navigator.clipboard.writeText(selectedKunde.code);
     setCopied(true);
-    setTimeout(() => setCopied(false), 2000); // Setze den Status nach 2 Sekunden zurück
+    setTimeout(() => setCopied(false), 2000);
   };
 
   if (loading) {
@@ -86,11 +104,23 @@ const Vertrag = () => {
             <p className="vertrag-text">Der Auftraggeber hat das Recht, die Anforderungen an die Dienstleistungen jederzeit zu ändern, sofern dies schriftlich mitgeteilt wird. Änderungsanforderungen können sich auf Design, Funktionalität, Inhalte und weitere Aspekte der Dienstleistung beziehen.</p>
             <p className="vertrag-text">Der Auftragnehmer verpflichtet sich, unverzüglich auf Änderungsanforderungen zu reagieren und eine schriftliche Einschätzung der Auswirkungen auf den Umfang, die Zeitplanung und die Leistungen zu erstellen.</p>
 
-            {selectedKunde.auftragsTyp === 'Webseite' && (
+            {selectedKunde.auftragsTyp === 'Webseite Programmieren' && (
               <>
                 <p className="vertrag-text">3. Admin-Rolle und Bearbeitung der Webseite</p>
                 <p className="vertrag-text">Der Auftraggeber erhält das Recht, die Webseite in seiner Funktion als Administrator zu bearbeiten und Änderungen vorzunehmen. Dies umfasst die Möglichkeit, Inhalte, Bilder, Texte, Videos und andere Informationen auf der Webseite eigenständig zu verwalten und zu aktualisieren.</p>
                 <p className="vertrag-text">Der Auftragnehmer wird dem Auftraggeber eine benutzerfreundliche und sichere Admin-Oberfläche zur Verfügung stellen, die es dem Auftraggeber ermöglicht, Änderungen an der Webseite vorzunehmen, ohne die Integrität oder Funktionsfähigkeit der Webseite zu beeinträchtigen.</p>
+                <p className="vertrag-text">4. Rücktrittsrecht des Auftraggebers</p>
+                <p className="vertrag-text">Der Auftraggeber hat das Recht, vom Vertrag zurückzutreten, wenn der Auftragnehmer wesentliche Vertragsverpflichtungen nicht erfüllt. Im Falle eines Rücktritts wird eine angemessene Entschädigung für bereits erbrachte Leistungen fällig.</p>
+                <p className="vertrag-text">5. Einsicht in den Source Code</p>
+                <p className="vertrag-text">Der Auftraggeber hat das Recht, Einsicht in den Source Code der Webseite zu nehmen, um die Qualität und Übereinstimmung der Programmierung zu überprüfen. Der Zugriff auf den Source Code wird dem Auftraggeber in einem angemessenen Umfang gewährt.</p>
+                <p className="vertrag-text">6. Berichtspflicht</p>
+                <p className="vertrag-text">Der Auftragnehmer verpflichtet sich, dem Auftraggeber regelmäßig über den Fortschritt der Arbeiten zu berichten. Diese Berichte umfassen den Stand der Umsetzung, eventuelle Probleme und die voraussichtliche Fertigstellung.</p>
+                <p className="vertrag-text">7. Vertraulichkeit</p>
+                <p className="vertrag-text">Der Auftragnehmer verpflichtet sich, alle vom Auftraggeber bereitgestellten Informationen und Daten vertraulich zu behandeln und diese nicht ohne ausdrückliche Zustimmung des Auftraggebers an Dritte weiterzugeben.</p>
+                <p className="vertrag-text">8. Urheberrecht und Eigentum</p>
+                <p className="vertrag-text">Alle Urheberrechte und geistigen Eigentumsrechte an der von dem Auftragnehmer erstellten Webseite verbleiben bis zur vollständigen Bezahlung beim Auftragnehmer. Nach vollständiger Bezahlung gehen die Rechte an der Webseite und ihren Inhalten auf den Auftraggeber über.</p>
+                <p className="vertrag-text">9. Vergütung</p>
+                <p className="vertrag-text">Die Vergütung für die Erstellung und Betreuung der Webseite wird in Anlage 2 festgelegt. Zahlungen sind gemäß dem vereinbarten Zahlungsplan zu leisten. Bei Zahlungsverzug behält sich der Auftragnehmer vor, die Arbeit bis zur Begleichung der ausstehenden Beträge einzustellen.</p>
               </>
             )}
             
